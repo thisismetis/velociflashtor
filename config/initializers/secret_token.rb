ea663f55e976eb67eab4a602a2cd667a8f942f8a
Jatin Bhandari <jatin.bhandari@mail.utoronto.ca>
2014-04-14 09:52:14 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Velociflashtor::Application.config.secret_key_base = '2ff240114d8300303494e48dea295bdb1c9fc77135c1aed1c372e2cdb2c3d7010fb9a43f5a2c3a6c33d7f029f04180b02a82f1de82c1c0f94132a79c37b8f8f5'
