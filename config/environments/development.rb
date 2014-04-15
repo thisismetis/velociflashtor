808ac1d8887e65947dad0f17f22d866b23902528
Derek Kaknes <derekkaknes@derek.local>
2014-04-15 10:01:11 -0400

    Implements "Add Images" feature to the card creation process. Implementation uses google-search gem to retrieve three images from Google using the front of the card as the query term.
    Initial implementation of Add Images
    Amends redirecting through Cards#create
    Refactors the Add Image feature

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,5 @@ Velociflashtor::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer.default_url_options = host: 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
