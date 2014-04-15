f99dcdc473ca39f1201f67ad7e3a8b16b8461958
Adam Patterson <Adam Patterson>
2014-04-15 14:58:25 -0400

    created private find_tag method

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,14 @@
 class TagsController < ApplicationController
 
   def show
-    @tag = Tag.find(params[:id])
+    @tag = find_tag
     @decks = @tag.decks(deck: [:user])
   end
+
+  private
+
+  def find_tag
+    Tag.find(params[:id])
+  end
+
 end
