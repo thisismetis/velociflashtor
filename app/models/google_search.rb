10e24d32c5c37e5df4512a963297399e00baf816
Derek Kaknes <derekkaknes@derek.local>
2014-04-15 14:14:12 -0400

    Fix Hound comments

--- app/models/google_search.rb
+++ app/models/google_search.rb
@@ -1,13 +1,13 @@
 class GoogleSearch
 
-  def self.google_search(query, num=3)
+  def self.google_search(query, num = 3)
     google_search = Google::Search::Image.new(query: query)
     results = google_search.response
-    images =[]
+    images = []
     results.each do |image|
       images << image.uri
     end
-    images[0..(num-1)]
+    images[0..(num - 1)]
   end
 
 end
