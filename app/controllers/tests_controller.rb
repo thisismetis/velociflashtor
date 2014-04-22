331edfa99b1457eda080d4b80fd8cd2a5f6bd51f
Jatin Bhandari <jatin.bhandari@mail.utoronto.ca>
2014-04-22 16:53:03 -0400

    Small updates

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,7 +3,6 @@ class TestsController < ApplicationController
     @deck = find_deck
     @card_ids = @deck.cards.ids.shuffle
     @card = Card.find(@card_ids.first)
-    @num_guesses = 0
     @card_last_id = @card_ids.last
   end
 end
