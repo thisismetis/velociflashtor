8f23edf7a2843e5525dd8da82dd4304023fc7eac
Jatin Bhandari <jatin.bhandari@mail.utoronto.ca>
2014-04-24 13:59:48 -0400

    fixes trailing whitespace

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ActiveRecord::Base
   belongs_to :user
-  belongs_to :deck, counter_cache: true 
+  belongs_to :deck, counter_cache: true
 
   validates :user_id, uniqueness: { scope: :deck_id }
 end
