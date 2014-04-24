2feb11ed93944b1758b73f5c9904e96159b69590
Derek Kaknes <derekkaknes@derek.local>
2014-04-24 09:32:10 -0400

    Adds Results page with guess_bank

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,12 +1,14 @@
 class ResultsController < ApplicationController
+  respond_to :json
 
   def index
+    @card_ids = card_ids
     @deck = find_deck
     @guess_bank = guess_bank
   end
 
   def show
-    @guess_bank = params[:guess_bank]
+    @guess_bank = guess_bank
     @deck = Deck.find(params[:id])
     @like = Like.find_by(deck: @deck)
   end
@@ -14,10 +16,16 @@ class ResultsController < ApplicationController
   private
 
   def find_deck
-    Deck.find(params[:guess][:deck_id])
+    Deck.find(params[:deck_id])
   end
 
   def guess_bank
-    params[:guess][:guess_bank]
+    guess_hash = params[:guess_bank].dup
+    guess_hash.inject({}) { |h, (k, v)| h[k.to_i] = v; h }
+  end
+
+  def card_ids
+    string_array = params[:card_ids]
+    string_array.map(&:to_i)
   end
 end
