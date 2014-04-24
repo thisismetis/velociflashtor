e86db5dc8b84cae5b67c3cf49acc841e1253543f
Jatin Bhandari <jatin.bhandari@mail.utoronto.ca>
2014-04-24 13:46:44 -0400

    adds most recommended decks to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,8 @@
 class DashboardsController < ApplicationController
+
   def show
     @decks = current_user.decks.order(created_at: :desc).last(5)
+    @most_recommended = Deck.order(likes_count: :desc).first(5)
   end
+
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 class Like < ActiveRecord::Base
   belongs_to :user
-  belongs_to :deck
+  belongs_to :deck, counter_cache: true 
 
-  validates :user_id,
-        uniqueness: { scope: :deck_id }
+  validates :user_id, uniqueness: { scope: :deck_id }
 end
