ee6096fdfa954efca94df54bbe4e7c26259e43c7
Derek Kaknes <derekkaknes@derek.local>
2014-04-24 11:46:12 -0400

    Minor edits from Goose

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,19 +4,16 @@ class ResultsController < ApplicationController
   def show
     @deck = find_deck
     @like = Like.find_by(deck: @deck)
-    @results = []
-    guess_bank.each do |card_id, guesses|
+    @results = guess_bank.map do |card_id, guesses|
       card = Card.find(card_id)
       result = Result.new(card, guesses)
-      @results << result
     end
   end
 
   private
 
   def guess_bank
-    guess_hash = params[:guess_bank]
-    guess_hash.inject({}) { |h, (k, v)| h[k.to_i] = v; h }
+    params[:guess_bank]
   end
 
 end
